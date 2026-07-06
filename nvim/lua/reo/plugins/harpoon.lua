be4488f071598774097a243f114af9e660863403
Redooyyy <rwarieal@gmail.com>
2026-07-07 02:53:12 +0600

    feat(nvim): add various plugins for ui, formatting, utilities and treesitter

---
+++ nvim/lua/reo/plugins/harpoon.lua
@@ -0,0 +1,25 @@
+return {
+  "ThePrimeagen/harpoon",
+
+  config = function()
+    local mark = require("harpoon.mark")
+    local ui = require("harpoon.ui")
+
+    --just saving streaks -_-
+    --harpoon (swap between files)--
+    vim.keymap.set("n", "<leader>a", mark.add_file)
+    vim.keymap.set("n", "<C-a>", ui.toggle_quick_menu)
+    vim.keymap.set("n", "<C-u>", function()
+      ui.nav_file(1)
+    end)
+    vim.keymap.set("n", "<C-i>", function()
+      ui.nav_file(2)
+    end)
+    vim.keymap.set("n", "<C-o>", function()
+      ui.nav_file(3)
+    end)
+    vim.keymap.set("n", "<C-p>", function()
+      ui.nav_file(4)
+    end)
+  end,
+}
